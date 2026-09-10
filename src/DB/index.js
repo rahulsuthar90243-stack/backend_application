7fb58bf2680805e76330fd7484b9f3151eb9c22f
Rahul <rahulsuthar90243@gmail.com>
2026-09-10 23:32:12 +0530

    improve MongoDB connection handling

--- src/DB/index.js
+++ src/DB/index.js
@@ -1,18 +1,23 @@
-// second approach
-import mongoose, { connect } from "mongoose";
-import DB_Name from "../constants.js"
+import mongoose from "mongoose";
+import DB_Name from "../constants.js";
 
 
 const connectDB  = async () =>{
   try {
+    if (!process.env.MONGODB_URL) {
+      throw new Error("MONGODB_URL is not defined in the environment");
+    }
 
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_Name}`);
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_Name}`,
+      { serverSelectionTimeoutMS: 10000 }
+    );
     console.log(`\n MONGODB Connected !! DB HOST ${connectionInstance.connection.host}`);
     
     
   } catch (error) {
-    console.log("MONGODB Connection Error: ", error);
-    process.emit(1);
+    console.error("MONGODB Connection Error:", error);
+    throw error;
   }
 }
 
