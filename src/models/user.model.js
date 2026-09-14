61a728c8ee83ebf179d48314760ef66aef3ff3ea
Rahul <rahulsuthar90243@gmail.com>
2026-09-14 22:40:25 +0530

    user model fixe buges and error

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,94 +1,93 @@
-import mongoose, {Schema} from "mongoose";
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true,
-            index: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true,
-        },
-        fullname: {
-            type: String,
-            required: true,
-            trim: true,
-            index: true
-        },
-        avatar: {
-            type: String,
-            required: true,
-        },
-        coverImage: {
-            trype: String,
-        },
-        watchHistory: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "Video"
-            }
-        ],
-        password: {
-            type: String,
-            required: [true, "Password is Required"]
-        },
-        refreshToken: {
-            type: String
-        },
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
-    {timestamps: true}
-)
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-userSchema.pre("save", async function(next) {   //pre-save hook to hash the password before saving the user document to the database.
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-    if(!this.modified('password')) return next();
-
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-
-})
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return bcrypt.compare(password, this.password)
-}
+  return bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.generateAccessToken = function() {
-   return jwt.sign(
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
     {
-        _id: this._id,
-        email: this.email,
-        username: this.username,
-        fullname: this.fullname
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-   )
-}
-userSchema.methods.generateRefreshToken = function() {
-    return jwt.sign(
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
     {
-        _id: this._id,
-        email: this.email,
-        username: this.username,
-        fullname: this.fullname
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
-        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
-   )
-}
-
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
