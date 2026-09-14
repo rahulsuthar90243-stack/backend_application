cec96d14f42ee95aa1e5c28c7d295d3d51315851
Rahul <rahulsuthar90243@gmail.com>
2026-09-14 22:39:38 +0530

    fixe mongoDB connect error

--- src/DB/index.js
+++ src/DB/index.js
@@ -1,24 +1,23 @@
 import mongoose from "mongoose";
 import DB_Name from "../constants.js";
 
-
-const connectDB  = async () =>{
+const connectDB = async () => {
   try {
     if (!process.env.MONGODB_URL) {
       throw new Error("MONGODB_URL is not defined in the environment");
     }
 
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URL}/${DB_Name}`,
-      { serverSelectionTimeoutMS: 10000 }
-    );
+    const mongoURI = `${process.env.MONGODB_URL.replace(/\/+$/, "")}/${DB_Name}`;
+
+    const connectionInstance = await mongoose.connect(mongoURI, {
+      serverSelectionTimeoutMS: 10000,
+    });
+
     console.log(`\n MONGODB Connected !! DB HOST ${connectionInstance.connection.host}`);
-    
-    
   } catch (error) {
     console.error("MONGODB Connection Error:", error);
     throw error;
   }
-}
+};
 
 export default connectDB
